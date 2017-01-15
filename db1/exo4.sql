9675a13230acdb75be3f8edad8086e4ffbeed93a
Totoro le Pacha <totorolepacha@gmail.com>
2017-01-15 17:37:13 +0100

    Exercice 3 et 4

---
+++ db1/exo4.sql
@@ -0,0 +1,6 @@
+create synonym personnel for personnel@hopit;
+create synonym medecin for medecin@hopit;
+create synonym infirmiere for infirmiere@hopit;
+create synonym rattache for rattache@hopit;
+create synonym affectation for affectation@hopit;
+create synonym service for service@hopit;
---
+++ db2/exo4.sql
@@ -0,0 +1,6 @@
+create synonym Personne for Personne@central;
+create synonym Hopital for Hopital@central;
+create synonym Integre for Integre@central;
+create synonym Patient for Patient@central;
+create synonym Externe for Externe@central;
+create synonym Hospitalise for Hospitalise@central;
